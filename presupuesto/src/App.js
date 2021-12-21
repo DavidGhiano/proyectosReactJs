5c77398811d88f5539590e4aebfcf0899b19ef26
David Omar Ghiano <davidghiano@gmail.com>
2021-12-20 22:46:13 -0300

    advance del proyecto

--- presupuesto/src/App.js
+++ presupuesto/src/App.js
@@ -1,11 +1,58 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
+import Pregunta from './components/Pregunta'
+import Formulario from './components/Formulario'
+import Listado from './components/Listado'
+
 
 function App() {
+
+  // definir States
+  const [presupuesto, guardarPresupuesto] = useState(0)
+  const [restante,guardarRestante] = useState(0);
+  const [mostrarPregunta, actualizarPregunta ] = useState(true);
+  const [gastos, guardarGastos] = useState([]);
+
+  // Cuando agreguemos un nuevo gastos
+  const agregarNuevoGasto = gasto =>{
+    guardarGastos([
+      ...gastos,
+      gasto
+    ])
+  }
+
+
   return (
     <Fragment>
       <div className="container">
         <header>
           <h1>Gasto semanal</h1>
+          <div className="contenido-principal contenido">
+            { mostrarPregunta ?
+              (
+                <Pregunta
+                  guardarPresupuesto={guardarPresupuesto}
+                  guardarRestante = {guardarRestante}
+                  actualizarPregunta = {actualizarPregunta}
+                />
+              )
+              : 
+              (
+                <div className="row">
+                  <div className="one-half column">
+                    <Formulario
+                      agregarNuevoGasto={agregarNuevoGasto}
+                    />
+                  </div>
+                  <div className="one-half column">
+                    <Listado
+                      gastos={gastos}
+                    />
+                  </div>
+                </div>
+              )
+            }
+
+          </div>
         </header>
       </div>
     </Fragment>
---
+++ presupuesto/src/components/Pregunta.js
@@ -0,0 +1,59 @@
+import React, { Fragment,useState } from 'react';
+import Error from './Error'
+
+const Pregunta = ({guardarPresupuesto, guardarRestante, actualizarPregunta}) => {
+
+    //Definir el state
+    const [cantidad, guardarCantidad] = useState(0);
+    const [error, guardarError] = useState(false);
+
+    const definirPresupuesto = e =>{
+        guardarCantidad(parseInt(e.target.value,10));
+    }
+
+    // Submit
+    const agregarPresupuesto = e=>{
+        e.preventDefault();
+
+        //validator
+        if(cantidad < 1 || isNaN(cantidad)){
+            guardarError(true);
+            return;
+        }
+        
+        //Si se pasa la validación
+        guardarError(false);
+        guardarPresupuesto(cantidad);
+        guardarRestante(cantidad);
+        actualizarPregunta(false);
+    }
+
+
+    return (  
+        <Fragment>
+            <h2>Coloca tu presupuesto</h2>
+
+            { error ? <Error mensaje="El presupuesto es incorrecto"/> :null}
+
+            <form
+                onSubmit={agregarPresupuesto}
+            >
+                <input
+                    type="number"
+                    className="u-full-width"
+                    placeholder="Coloca tu presupuesto"
+                    onChange={definirPresupuesto}
+                />
+                <input
+                    type="submit"
+                    className="u-full-width button-primary"
+                    value="Definir presupuesto"
+                />
+            </form>
+        </Fragment>
+
+
+    );
+}
+ 
+export default Pregunta;
