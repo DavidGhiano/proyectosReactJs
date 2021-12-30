b4a583c550b602ea05fd4831d2bde989a6cdf8e2
David Omar Ghiano <davidghiano@gmail.com>
2021-12-29 23:47:11 -0300

    Finalizando proyecto Clima

--- clima/src/App.js
+++ clima/src/App.js
@@ -24,7 +24,7 @@ function App() {
     const consultarAPi = async () => {
       if(consultar){
         const appId = "13aa364d36f81613570f7ec3602101ca";
-        const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
         
         const respuesta = await fetch(url);
         const resultado = await respuesta.json();
