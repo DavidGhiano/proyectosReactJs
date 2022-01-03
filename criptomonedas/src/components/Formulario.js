f16654b6e65f1b688d37a468fb743f2c6b02179d
David Omar Ghiano <davidghiano@gmail.com>
2022-01-02 21:03:41 -0300

    agregando hooks y styledç

--- criptomonedas/src/components/Formulario.js
+++ criptomonedas/src/components/Formulario.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import styled from '@emotion/styled';
 
+import useMoneda from '../hooks/useMoneda'
+
 const Boton = styled.input`
     margin-top: 20px;
     font-weight: bold;
@@ -21,10 +23,20 @@ const Boton = styled.input`
 
 
 const Formulario = () => {
+
+    const MONEDAS = [
+        {codigo: 'USD', nombre: 'Dolar de Estados Unidos'},
+        {codigo: 'MXN', nombre: 'Peso Mexicano'},
+        {codigo: 'EUR', nombre: 'Euro'},
+        {codigo: 'GBR', nombre: 'Libra Esterlina'},
+        {codigo: 'ARS', nombre: 'Peso Argentino'}
+    ];
+    // Utlizar useMoneda    
+    const [ moneda, SelectMonedas] = useMoneda('Elige tu Moneda','',MONEDAS);
+
     return (
         <form>
-            
-
+            <SelectMonedas/>
             <Boton
                 type="submit"
                 value="Calcular"
